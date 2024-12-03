d67fd4c74dd108710e5325f932422f89f93ff426
Grobik228 <lednikoviperiod@tutamail.com>
2024-12-03 14:34:27 +0300

    main

--- kod.py
+++ kod.py
@@ -11,14 +11,18 @@ def rasnost(a, b):
 
 def delenie(a, b):
     if b == 0:
-        return "Ошибка: деление на ноль"
+        return "Ошибка"
     return a / b
 
 def separate(string: str, separator):
     result = string.split(separator)
-    return [int(x) for x in result] 
+    return [float(x) for x in result] 
 
-if "+" in user:
+if "*" in user:
+    numbers = separate(user, "*")
+    print(umnoschenie(numbers[0], numbers[1]))
+
+elif "+" in user:
     numbers = separate(user, "+")
     print(summa(numbers[0], numbers[1]))
 
@@ -26,13 +30,9 @@ elif "-" in user:
     numbers = separate(user, "-")
     print(rasnost(numbers[0], numbers[1]))
 
-elif "*" in user:
-    numbers = separate(user, "*")
-    print(umnoschenie(numbers[0], numbers[1]))
-
 elif "/" in user:
     numbers = separate(user, "/")
     print(delenie(numbers[0], numbers[1]))
 
 else:
-    print('Неправильный ввод')
+    print('Неправильная операция')
